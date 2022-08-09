b41fe716afa9501006e01da0f67f8e04b92864db
Kevin Avicenna Widiarto <kevinavicennaa@protonmail.com>
2022-08-09 18:10:25 +0700

    Merapikan v1.0

--- UAS/MENCOBA UAS/tesregex.py
+++
@@ -1,9 +0,0 @@
-import re 
-
-#teks = 'kevin123'
-cocok = re.findall(r'\d\d','t123h di 2019 bulan 023')
-
-if cocok:
-    print('menemukan bruh',cocok)
-else:
-    print('ranek')
--- test2.py
+++
@@ -1,9 +0,0 @@
-fullstring = "StackAbuse"
-substring = "tack"
-
-try:
-    fullstring.index(substring)
-except ValueError:
-    print("Not found!")
-else:
-    print("Found!")
